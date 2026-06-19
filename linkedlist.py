4a420c2a3738616fd144adb69d3d2d01d02daf9d
Leonal Robin D <leonal2003lr10@gmail.com>
2026-06-19 18:29:26 +0530

    feat: add search, indexing, and insertion methods to LinkedList and update countll return type

--- linkedlist.py
+++ linkedlist.py
@@ -1,3 +1,4 @@
+from decimal import HAVE_THREADS
 class Node:
     def __init__(self,data):
         self.data = data
@@ -12,17 +13,41 @@ class Solution:
             ele.append(curr.data)
             curr = curr.next
         return ele
-    def countll(self, head:Node)-> str:
+    def countll(self, head:Node)-> int:
         count = 0
         curr = head 
         while curr:
             count +=1
             curr = curr.next
         return count
+    def searchll(self, head, ele:int):
+        curr = head
+        while curr:
+            if curr.data == ele:
+                return ele
+            curr=curr.next
+        return -1
 
+    def getele(self,head,index):
+        curr = head
+        count = 0
+        while curr:
+            if count == index:
+                return curr.data
+            curr = curr.next
+            count+=1
+        return -1   
+    
+    def insertll(self, data, head):
+        new_node = Node(data)
+        new_node.next = head
+        return new_node
+
+    
 ll = Node(10)
 ll.next = Node(20)
 ll.next.next = Node(30)
 sol = Solution()
 print(sol.traverse(ll))
 print(sol.countll(ll))
+print(sol.searchll(20))
